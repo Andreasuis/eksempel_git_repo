3c8832a1c4c191f27468b3f24758d05706e8396f
Andreasuis <283282@uis.no>
2024-09-16 14:48:47 +0200

    test
    Endring test

---
+++ turtle eksempel fra forelesning.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 14:38:20 2024
+
+@author: andre
+"""
+
+import turtle 
+
+import math 
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(50)
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+turtle.pensize(5)
+turtle.color("orange")
+turtle.forward(150)
+
+
+
+turtle.done()
