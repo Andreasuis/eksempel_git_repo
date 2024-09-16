9066aed6eebc23f82286b4e0853274f69aea385a
Andreasuis <283282@uis.no>
2024-09-16 14:58:06 +0200

    Fjernet strek

--- turtle eksempel fra forelesning.py
+++ turtle eksempel fra forelesning.py
@@ -9,12 +9,7 @@ import turtle
 
 import math 
 
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(50)
-turtle.penup()
-turtle.forward(50)
-turtle.pendown()
+
 turtle.pensize(5)
 turtle.color("orange")
 turtle.forward(150)
